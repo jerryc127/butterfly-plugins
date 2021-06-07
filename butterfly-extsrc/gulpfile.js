0172ce9e85e33d1ee1cd77149458bb46a42fcf64
Jerry <hwy0127@gmail.com>
2021-06-07 16:32:36 +0800

    feat: update animejs to newest fix: hexo-butterfly-douban 多了 </div> 導致樣式錯亂

--- butterfly-extsrc/gulpfile.js
+++ butterfly-extsrc/gulpfile.js
@@ -1,17 +1,25 @@
 const gulp = require('gulp')
 const rename = require('gulp-rename')
-const terser = require('gulp-terser');
+const terser = require('gulp-terser')
+const inject = require('gulp-inject-string')
 
-// minify js 
+// gulp.task('inject:prepend', function () {
+//   gulp.src('src/example.html')
+//     .pipe(inject.prepend('<!-- Created: ' + Date() + ' -->\n'))
+//     .pipe(rename('prepend.html'))
+//     .pipe(gulp.dest('build'))
+// })
+
+// minify js
 gulp.task('compress', () =>
   gulp.src('./src/**/*.js')
     .pipe(terser())
+    // .pipe(inject.prepend('// Created: ' + new Date().toLocaleDateString('en-US') + ' \n'))
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(gulp.dest('./dist'))
 )
-
 
 // 執行 gulp 命令時執行的任務
 gulp.task('default', gulp.parallel(
