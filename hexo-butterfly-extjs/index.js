dbc3402ce919ecd29d9b55c8f214b7c4f1d04bb3
Jerry <hwy0127@gmail.com>
2023-10-09 22:48:26 +0800

    update

--- hexo-butterfly-extjs/index.js
+++ hexo-butterfly-extjs/index.js
@@ -8,10 +8,27 @@ hexo.extend.generator.register('pluginsSrc', () => {
   const { third_party_provider } = hexo.theme.config.CDN
   if (third_party_provider && third_party_provider !== 'local') return
 
-  const { version } = require((path.join(hexo.theme_dir, 'package.json')))
-  const btfVer = version.replace(/(^.*\..*)\..*/, '$1')
+  const compareVersions = (version1, version2) => {
+    const v1 = version1.split('.').map(Number)
+    const v2 = version2.split('.').map(Number)
+    
+    for (let i = 0; i < Math.max(v1.length, v2.length); i++) {
+      const num1 = v1[i] || 0
+      const num2 = v2[i] || 0
+      
+      if (num1 < num2) {
+        return -1
+      } else if (num1 > num2) {
+        return 1
+      }
+    }
+    return 0
+  }
 
-  if (btfVer < 4.8) {
+  const { version } = require((path.join(hexo.theme_dir, 'package.json')))
+  const limitVer = '4.8'
+
+  if (compareVersions(version, limitVer) < 0) {
     hexo.log.warn('Please update theme to V4.8.0 or higher')
     hexo.log.warn('Or install hexo-butterfly-extjs to old version')
     hexo.log.warn('npm install hexo-butterfly-extjs@1.3.4')
