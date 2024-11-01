d2040b43646118fdaeca9540259eadf4830e1fe8
myw <hwy0127@gmail.com>
2024-11-01 19:09:41 +0800

    feat: 更新插件源代码移動邏輯及依賴版本

--- hexo-butterfly-extjs/index.js
+++ hexo-butterfly-extjs/index.js
@@ -35,16 +35,29 @@ hexo.extend.generator.register('pluginsSrc', () => {
     process.exit(-1)
   }
 
+  hexo.log.info('Start to move the source code of the plugins to the public directory.')
+
   const plugins = yaml.load(readFileSync(path.join(hexo.theme_dir, '/plugins.yml')))
   const dataObj = []
   const errorObj = []
+
+  const getModulePath = moduleName => {
+    try {
+      const packagePath = require.resolve(moduleName)
+      // use regex to get the path of the node_modules
+      const match = packagePath.match(/(.*[\\/]node_modules)(?=[\\/])/)
+      return match ? match[0] : hexo.plugin_dir
+    } catch (error) {
+      return hexo.plugin_dir
+    }
+  }
 
   for (const value of Object.values(plugins)) {
     const fullPath = `${value.name}/${value.file}`
     try {
       dataObj.push({
         path: `pluginsSrc/${fullPath}`,
-        data: readFileSync(path.join(hexo.plugin_dir, fullPath))
+        data: readFileSync(path.join(getModulePath(value.name), fullPath))
       })
     } catch (error) {
       errorObj.push(`The file does not exist: ${fullPath}`)
@@ -60,12 +73,24 @@ hexo.extend.generator.register('pluginsSrc', () => {
   }
 
   // webfonts
-  const folders = {}
-
-  folders.fontawesome_free = '@fortawesome/fontawesome-free/webfonts'
-  folders.social_share = 'butterfly-extsrc/sharejs/dist/fonts'
-  folders.mathjax_font = 'mathjax/es5/output/chtml/fonts'
-  folders.katex_font = 'katex/dist/fonts'
+  const folders = {
+    fontawesome_free: {
+      name: '@fortawesome/fontawesome-free',
+      path: 'webfonts'
+    },
+    social_share: {
+      name: 'butterfly-extsrc',
+      path: 'sharejs/dist/fonts'
+    },
+    mathjax_font: {
+      name: 'mathjax',
+      path: 'es5/output/chtml/fonts'
+    },
+    katex_font: {
+      name: 'katex',
+      path: 'dist/fonts'
+    }
+  }
 
   const lookForFiles = (path, origin) => {
     readdirSync(path).forEach(sub => {
@@ -85,8 +110,10 @@ hexo.extend.generator.register('pluginsSrc', () => {
   }
 
   for (const value of Object.values(folders)) {
-    lookForFiles(path.join(hexo.plugin_dir, value), value)
+    const filePath = path.join(value.name, value.path)
+    lookForFiles(path.join(getModulePath(value.name), filePath), filePath)
   }
 
+  hexo.log.info('Move the source code of the plugins to the public directory successfully.')
   return dataObj
 })
